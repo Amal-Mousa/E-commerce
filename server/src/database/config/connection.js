ee26dc8a90e2fe56f31465709005c909152e8ed2
Amal-Mousa <amalalsattari@gmail.com>
2023-05-26 17:43:48 +0300

    set database connection

---
+++ server/src/database/config/connection.js
@@ -0,0 +1,15 @@
+const Pool = require('pg');
+require('dotenv').config();
+
+const { DEV_DB_URL, NODE_ENV, DB_URL } = process.env;
+
+const dbUrl = NODE_ENV === 'development' ? DEV_DB_URL : DB_URL;
+
+const options = {
+  connectionString: dbUrl,
+  ssl: NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+};
+
+const connection = new Pool(options);
+
+module.exports = { connection };
